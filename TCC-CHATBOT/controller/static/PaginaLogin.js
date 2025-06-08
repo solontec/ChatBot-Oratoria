7ae55f45a00b36dcf98b6cf135decef83fc5125e
Micro <guisolon.tec@gmail.com>
2025-06-08 00:14:33 -0300

    Commitadas para colocar o js quebrado do dudu

---
+++ TCC-CHATBOT/controller/static/PaginaLogin.js
@@ -0,0 +1,97 @@
+function toggleMode() {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
+}
+
+let texto = document.getElementById("texto-apresentacao-bem-vindo");
+let tamanhoBase = parseFloat(window.getComputedStyle(texto).fontSize);
+
+document.getElementById("increase-font").addEventListener("click", function () {
+let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
+
+let novoTamanho = tamanhoAtual * 1.1;
+
+if (novoTamanho > 32) {
+novoTamanho = 32
+}
+
+texto.style.fontSize = novoTamanho + "px";
+});
+
+
+document.getElementById("decrease-font").addEventListener("click", function () {
+    let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
+    if (tamanhoAtual > tamanhoBase * 0.8) { // Evita que fique muito pequeno
+        texto.style.fontSize = (tamanhoAtual * 0.9) + "px"; // Diminui 10%
+    }
+
+    
+}); 
+
+        const entrar = document.getElementById('entrar');
+        const rm = document.getElementById('rm');
+        const senha = document.getElementById('senha');
+
+        entrar.addEventListener('click', () => {
+            if (rm.value === '123' && senha.value === '123') {
+                window.location.href = '../PaginaInicial/TelaInicial.html';
+            } else {
+                alert('Usuário ou senha inválidos');
+            };
+        });
+
+let posicaoAtual = 0;
+
+function moverContainer(direcao) {
+    const containerLogin = document.getElementById('container-login');
+    const containerCadastro = document.getElementById('container-cadastro');
+
+    if (direcao === 'direita' && posicaoAtual < 1) {
+        posicaoAtual++;
+    } else if (direcao === 'esquerda' && posicaoAtual > -1) {
+        posicaoAtual--;
+    }
+
+    if (posicaoAtual === 0) {
+        containerLogin.style.transform = 'translateX(0) translateY(-50%)';
+        containerCadastro.style.transform = 'translateX(0) translateY(-50%)';
+    } else if (posicaoAtual === 1) {
+        containerLogin.style.transform = 'translateX(768px) translateY(-50%)';
+        containerCadastro.style.transform = 'translateX(-857px) translateY(-50%) scale(1.10)';
+    }
+}
+
+function moverContainer2(direcao) {
+    const containerLogin = document.getElementById('container-login');
+    const senhaEsquecida = document.getElementById('senha-esquecida');
+
+    if (direcao === 'direita' && posicaoAtual < 1) {
+        posicaoAtual++;
+    } else if (direcao === 'esquerda' && posicaoAtual > -1) {
+        posicaoAtual--;
+    }
+
+    if (posicaoAtual === 0) {
+        containerLogin.style.transform = 'translateX(0) translateY(-50%)';
+        senhaEsquecida.style.transform = 'translateX(0) translateY(-50%)';
+    } else if (posicaoAtual === 1) {
+        containerLogin.style.transform = 'translateX(768px) translateY(-50%)';
+        senhaEsquecida.style.transform = 'translateX(-857px) translateY(-55%) scale(1.10)';
+    }
+}
+
+function mostrarSenha() {
+  const input = document.getElementById("senha");
+  const icon = document.getElementById("toggleSenha");
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.add("fa-eye");
+    icon.classList.remove("fa-eye-slash");
+  } else {
+    input.type = "password";
+    icon.classList.add("fa-eye-slash");
+    icon.classList.remove("fa-eye");
+
+  }
+}
